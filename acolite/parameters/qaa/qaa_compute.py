c41ba14873fc091e9fb40a284ef6ed175854f48b
acolite <quinten.vanhellemont@naturalsciences.be>
2021-03-30 21:24:58 +0200

    Changed QAA parameter names

--- acolite/parameters/qaa/qaa_compute.py
+++ acolite/parameters/qaa/qaa_compute.py
@@ -3,7 +3,7 @@
 
 def qaa_compute(qaa_in, sza = 0, satellite = None,
                 qaa_wave = [443, 490, 560, 665],qaa_coef = None,
-                compute_zeu_lee = False, k_atag = 'a490_v6',k_bbptag = 'bbp490_v6'):
+                compute_zeu_lee = False, k_atag = 'v6_a_490',k_bbptag = 'v6_bbp_490'):
 
     import time
     import numpy as np
@@ -26,30 +26,30 @@ def qaa_compute(qaa_in, sza = 0, satellite = None,
 
         ## step 0
         ## compute Lee's subsurface rrs
-        rname = 'rrs{}'.format(k)
+        rname = 'rrs_{}'.format(k)
         qaa_data[rname] = qaa_in[k]/(0.52+(1.7*qaa_in[k]))
 
         ## step 1
         ## compute Lee's u
-        uname = 'u{}'.format(k)
+        uname = 'u_{}'.format(k)
         qaa_data[uname] = (-1.0*qaa_coef['g'][0] + np.sqrt((np.power(qaa_coef['g'][0],2.)+(4.*qaa_coef['g'][1]*qaa_data[rname]))))/(2.*qaa_coef['g'][1])
 
     ## step 2
     ## get rhow and a560 according to QAA5
-    rhow_v5=np.log10((qaa_data['rrs443']+qaa_data['rrs490'])/\
-                        (qaa_data['rrs560']+(5.*qaa_data['rrs665']*(qaa_data['rrs665']/qaa_data['rrs490']))))
-    qaa_data['a560_v5'] = qaa_coef['aw'][2]+np.power(10.,(qaa_coef['h'][2]+(qaa_coef['h'][1]*rhow_v5)+qaa_coef['h'][0]*np.power(rhow_v5,2.)))
+    rhow_v5=np.log10((qaa_data['rrs_443']+qaa_data['rrs_490'])/\
+                        (qaa_data['rrs_560']+(5.*qaa_data['rrs_665']*(qaa_data['rrs_665']/qaa_data['rrs_490']))))
+    qaa_data['v5_a_560'] = qaa_coef['aw'][2]+np.power(10.,(qaa_coef['h'][2]+(qaa_coef['h'][1]*rhow_v5)+qaa_coef['h'][0]*np.power(rhow_v5,2.)))
 
     ## get rhow and a665 according to QAA6
-    rhow_v6=qaa_data['rrs665']/(qaa_data['rrs443']+qaa_data['rrs490'])
-    qaa_data['a665_v6']= qaa_coef['aw'][3]+qaa_coef['k'][0]*np.power(rhow_v6,qaa_coef['k'][1])
+    rhow_v6=qaa_data['rrs_665']/(qaa_data['rrs_443']+qaa_data['rrs_490'])
+    qaa_data['v6_a_665']= qaa_coef['aw'][3]+qaa_coef['k'][0]*np.power(rhow_v6,qaa_coef['k'][1])
 
     ## step 3
-    qaa_data['bbp560_v5'] = ((qaa_data['u560'] * qaa_data['a560_v5']) / (1.0 - qaa_data['u560'])) - qaa_coef['bbw'][2]
-    qaa_data['bbp665_v6'] = ((qaa_data['u665'] * qaa_data['a665_v6']) / (1.0 - qaa_data['u665'])) - qaa_coef['bbw'][3]
+    qaa_data['v5_bbp_560'] = ((qaa_data['u_560'] * qaa_data['v5_a_560']) / (1.0 - qaa_data['u_560'])) - qaa_coef['bbw'][2]
+    qaa_data['v6_bbp_665'] = ((qaa_data['u_665'] * qaa_data['v6_a_665']) / (1.0 - qaa_data['u_665'])) - qaa_coef['bbw'][3]
 
     ## step 4 QAAv6
-    ratio = qaa_data['rrs443']/qaa_data['rrs560']
+    ratio = qaa_data['rrs_443']/qaa_data['rrs_560']
     Y = qaa_coef['l'][0] * (1.0 - qaa_coef['l'][1] * np.exp(qaa_coef['l'][2]*ratio))
 
     ## compute a QAA5 & QAA6
@@ -59,15 +59,15 @@ def qaa_compute(qaa_in, sza = 0, satellite = None,
         refn = {'5':560, '6':665}[version]
         for iw,wave in enumerate(qaa_wave):
             ## get dataset tags
-            bbptag = 'bbp{}_v{}'.format(wave, version)
-            atag = 'a{}_v{}'.format(wave, version)
-            utag = 'u{}'.format(wave)
-            kdtag = 'Kd{}_v{}'.format(wave, version)
+            bbptag = 'v{}_bbp_{}'.format(version,wave)
+            atag = 'v{}_a_{}'.format(version,wave)
+            utag = 'u_{}'.format(wave)
+            kdtag = 'v{}_Kd_{}'.format(version,wave)
 
             ## compute bbp and q
             if not (((version == '5') & (wave == 560)) or ((version == '6') & (wave == 665))):
                 ##step 5 - get bbp
-                qaa_data[bbptag] = qaa_data['bbp{}_v{}'.format(refn,version)] * \
+                qaa_data[bbptag] = qaa_data['v{}_bbp_{}'.format(version,refn)] * \
                                        np.power(float(refw)/float(wave),Y)
                 ##step 6 - get a
                 qaa_data[atag] = ((1.0 - qaa_data[utag]) * (qaa_coef['bbw'][iw]+qaa_data[bbptag])) / qaa_data[utag]
@@ -80,16 +80,16 @@ def qaa_compute(qaa_in, sza = 0, satellite = None,
     ## get switched datasets
     for iw,wave in enumerate(qaa_wave):
         for qaapar in ['a','bbp','Kd']:
-            t5 = '{}{}_v5'.format(qaapar,wave)
-            t6 = '{}{}_v6'.format(qaapar,wave)
+            t5 = 'v5_{}_{}'.format(qaapar,wave)
+            t6 = 'v6_{}_{}'.format(qaapar,wave)
             #tsw = '{}{}_vw'.format(qaapar,wave)
             #qaa_data[tsw] = qaa_data[t6]
             #if len(v5_idx[0]>0): qaa_data[tsw][v5_idx]=qaa_data[t5][v5_idx]
             if len(v5_idx[0]>0): qaa_data[t6][v5_idx]=qaa_data[t5][v5_idx]
 
     ## parameters for KdPAR 0-1m Nechad KdPARv2
-    qaa_data['KdPAR_Nechad_v5']=1.2529*np.log(1.+qaa_data['Kd490_v5'])+0.1127
-    qaa_data['KdPAR_Nechad_v6']=1.2529*np.log(1.+qaa_data['Kd490_v6'])+0.1127
+    qaa_data['v5_KdPAR_Nechad']=1.2529*np.log(1.+qaa_data['v5_Kd_490'])+0.1127
+    qaa_data['v6_KdPAR_Nechad']=1.2529*np.log(1.+qaa_data['v6_Kd_490'])+0.1127
 
     ## get Zeu
     c1=[-0.057,0.482,4.221]
@@ -105,8 +105,8 @@ def qaa_compute(qaa_in, sza = 0, satellite = None,
 
     # Kpar, Lee et al. 2007
     z = 1.
-    qaa_data['KdPAR_Lee_v6'] = k1+(k2)/np.sqrt(z+1.)
-    qaa_data['Zeu_Lee_v6'] = 4.6/qaa_data['KdPAR_Lee_v6']
+    qaa_data['v6_KdPAR_Lee'] = k1+(k2)/np.sqrt(z+1.)
+    qaa_data['v6_Zeu_Lee'] = 4.6/qaa_data['v6_KdPAR_Lee']
 
     ## slow (root solving)
     if compute_zeu_lee:
@@ -121,7 +121,7 @@ def qaa_compute(qaa_in, sza = 0, satellite = None,
             i2=val[1][i]
             zeu[i1,i2] = np.real(np.roots((y1[i1,i2],y2[i1,i2],y3[i1,i2])))[1]
         zeu[zeu<0]=np.nan
-        qaa_data['Zeu_Lee_v6_roots'] = zeu
+        qaa_data['v6_Zeu_Lee_roots'] = zeu
 
     t1 = time.time()
     print('Finished QAA computations in {:.1f} seconds'.format(t1-t0))
