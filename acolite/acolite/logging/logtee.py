767e7cde2055337a6f8858c050898682857cf83d
acolite <quinten.vanhellemont@naturalsciences.be>
2021-06-10 11:03:34 +0200

    Provide warning if user output directory is not writable

--- acolite/acolite/logging/logtee.py
+++ acolite/acolite/logging/logtee.py
@@ -6,14 +6,21 @@ class LogTee(object):
             self.name=name
             ## make new file
             if os.path.exists(os.path.dirname(self.name)) is False:
-                os.makedirs(os.path.dirname(self.name))
+                try:
+                    os.makedirs(os.path.dirname(self.name))
+                except:
+                    print('Error: could not create directory: {}'.format(os.path.dirname(self.name)))
+                    exit(1)
             self.file = open(self.name, 'w')
             self.file.close()
             self.mode='a'
             self.stdout = sys.stdout
             sys.stdout = self
         def __del__(self):
-            sys.stdout = self.stdout
+            try:
+                sys.stdout = self.stdout
+            except:
+                pass
         def write(self, data):
             self.stdout.write(data)
             data = data.strip()
