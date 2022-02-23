968f90dcb12d643532aac991cfe9d4f852f8aa92
acolite <quinten.vanhellemont@naturalsciences.be>
2022-02-23 17:44:40 +0100

    Added option to output PRISMA L2C data in ACOLITE NetCDF format

--- acolite/prisma/l1_convert.py
+++ acolite/prisma/l1_convert.py
@@ -4,6 +4,7 @@
 ## 2021-07-14
 ## modifications: 2021-12-31 (QV) new handling of settings
 ##                2022-01-04 (QV) added netcdf compression
+##                2022-02-23 (QV) added option to output L2C reflectances
 
 def l1_convert(inputfile, output=None, settings = {}, verbosity=0):
     import numpy as np
@@ -170,6 +171,30 @@ def l1_convert(inputfile, output=None, settings = {}, verbosity=0):
             ac.output.nc_write(ofile, 'raa', np.flip(np.rot90(raa)),
                                 netcdf_compression=setu['netcdf_compression'], netcdf_compression_level=setu['netcdf_compression_level'])
 
+        ## store l2c data
+        store_l2c = setu['prisma_store_l2c']
+        store_l2c_separate_file = setu['prisma_store_l2c_separate_file']
+        if store_l2c & read_cube:
+            if store_l2c_separate_file:
+                obase_l2c  = '{}_{}_converted_L2C'.format('PRISMA',  time.strftime('%Y_%m_%d_%H_%M_%S'))
+                ofile_l2c = '{}/{}.nc'.format(odir, obase_l2c)
+                ac.output.nc_write(ofile_l2c, 'lat', np.flip(np.rot90(lat)), new=True, attributes=gatts,
+                                    netcdf_compression=setu['netcdf_compression'], netcdf_compression_level=setu['netcdf_compression_level'])
+                ac.output.nc_write(ofile_l2c, 'lon', np.flip(np.rot90(lon)),
+                                    netcdf_compression=setu['netcdf_compression'], netcdf_compression_level=setu['netcdf_compression_level'])
+            else:
+                ofile_l2c = '{}'.format(ofile)
+
+            ## get l2c details for reflectance conversion
+            h5_l2c_gatts = ac.prisma.attributes(l2file)
+            scale_max = h5_l2c_gatts['L2ScaleVnirMax']
+            scale_min = h5_l2c_gatts['L2ScaleVnirMin']
+
+            ##  read in data cube
+            with h5py.File(l2file, mode='r') as f:
+                vnir_l2c_data = f['HDFEOS']['SWATHS']['PRS_L2C_HCO']['Data Fields']['VNIR_Cube'][:]
+                swir_l2c_data = f['HDFEOS']['SWATHS']['PRS_L2C_HCO']['Data Fields']['SWIR_Cube'][:]
+
         ## write TOA data
         for bi, b in enumerate(bands):
             wi = bands[b]['index']
@@ -180,6 +205,8 @@ def l1_convert(inputfile, output=None, settings = {}, verbosity=0):
                 if read_cube:
                     cdata_radiance = vnir_data[:,wi,:]
                     cdata = cdata_radiance * (np.pi * d * d) / (bands[b]['f0'] * cossza)
+                    if store_l2c:
+                        cdata_l2c = scale_min + (vnir_l2c_data[:, wi, :] * (scale_max - scale_min)) / 65535
                 else:
                     cdata_radiance = h5_gatts['Offset_Vnir'] + \
                             f['HDFEOS']['SWATHS']['PRS_L1_{}'.format(src)]['Data Fields']['VNIR_Cube'][:,i,:]/h5_gatts['ScaleFactor_Vnir']
@@ -189,6 +216,8 @@ def l1_convert(inputfile, output=None, settings = {}, verbosity=0):
                 if read_cube:
                     cdata_radiance = swir_data[:,wi,:]
                     cdata = cdata_radiance * (np.pi * d * d) / (bands[b]['f0'] * cossza)
+                    if store_l2c:
+                        cdata_l2c = scale_min + (swir_l2c_data[:, wi, :] * (scale_max - scale_min)) / 65535
                 else:
                     cdata_radiance = h5_gatts['Offset_Swir'] + \
                             f['HDFEOS']['SWATHS']['PRS_L1_{}'.format(src)]['Data Fields']['SWIR_Cube'][:,i,:]/h5_gatts['ScaleFactor_Swir']
@@ -203,6 +232,7 @@ def l1_convert(inputfile, output=None, settings = {}, verbosity=0):
                               netcdf_compression=setu['netcdf_compression'],
                               netcdf_compression_level=setu['netcdf_compression_level'],
                               netcdf_compression_least_significant_digit=setu['netcdf_compression_least_significant_digit'])
+                cdata_radiance = None
 
             ## write toa reflectance
             ac.output.nc_write(ofile, 'rhot_{}'.format(bands[b]['wave_name']), np.flip(np.rot90(cdata)),
@@ -210,5 +240,19 @@ def l1_convert(inputfile, output=None, settings = {}, verbosity=0):
                               netcdf_compression=setu['netcdf_compression'],
                               netcdf_compression_level=setu['netcdf_compression_level'],
                               netcdf_compression_least_significant_digit=setu['netcdf_compression_least_significant_digit'])
+            cdata = None
+            print('Wrote rhot_{}'.format(bands[b]['wave_name']))
+
+            ## store L2C data
+            if store_l2c & read_cube:
+                ac.output.nc_write(ofile_l2c, 'rhos_l2c_{}'.format(bands[b]['wave_name']), np.flip(np.rot90(cdata_l2c)),
+                                  dataset_attributes = ds_att,
+                                  netcdf_compression=setu['netcdf_compression'],
+                                  netcdf_compression_level=setu['netcdf_compression_level'],
+                                  netcdf_compression_least_significant_digit=setu['netcdf_compression_least_significant_digit'])
+                ofile_l2c_new = False
+                cdata_l2c = None
+                print('Wrote rhos_l2c_{}'.format(bands[b]['wave_name']))
+
         ofiles.append(ofile)
     return(ofiles, setu)
