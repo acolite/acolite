97ae5bdf07ffafadce4d43787910c2f51d513e33
acolite <quinten.vanhellemont@naturalsciences.be>
2022-04-05 14:22:41 +0200

    Added mean VAA and SAA to gatts

--- acolite/sentinel2/l1_convert.py
+++ acolite/sentinel2/l1_convert.py
@@ -170,7 +170,7 @@ def l1_convert(inputfile, output = None, settings = {},
 
         ## make global attributes for L1R NetCDF
         gatts = {'sensor':sensor, 'isodate':isodate, 'global_dims':global_dims,
-                 'sza':sza, 'vza':vza, 'raa':raa, 'se_distance': se_distance,
+                 'sza':sza, 'vza':vza, 'raa':raa, 'vaa': vaa, 'saa': saa, 'se_distance': se_distance,
                  'mus': np.cos(sza*(np.pi/180.)), 'granule': granule, 'mgrs_tile': mgrs_tile,
                  'acolite_file_type': 'L1R'}
         if merge_tiles:
