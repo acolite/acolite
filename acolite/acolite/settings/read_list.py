e4917c269b8853ba3a2b3b4b032dfeb477e574cb
acolite <quinten.vanhellemont@naturalsciences.be>
2022-04-12 16:47:56 +0200

    Skip empty lines

--- acolite/acolite/settings/read_list.py
+++ acolite/acolite/settings/read_list.py
@@ -9,6 +9,7 @@ def read_list(file):
     with open(file, 'r', encoding='utf-8') as f:
         for il, line in enumerate(f.readlines()):
             line = line.strip()
+            if len(line) == 0: continue # skip empty lines
             if line[0] in ['#', ';']: continue
             data_list.append(line)
     return(data_list)
