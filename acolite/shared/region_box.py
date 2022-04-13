d9b24c0c02baddd82960755026acd7ee9d52f6dc
acolite <quinten.vanhellemont@naturalsciences.be>
2022-04-13 12:22:28 +0200

    Added return limit

--- acolite/shared/region_box.py
+++ acolite/shared/region_box.py
@@ -2,16 +2,13 @@
 ## makes json box of x km around given point
 ## written by Quinten Vanhellemont, RBINS
 ## 2021-03-02
-## modifications:
+## modifications:  2022-04-13 (QV) added return limit option
 ##
 
-def region_box(site, site_lon, site_lat, box_size = 3, add_box_name = True, sub_dir = None, override=True):
+def region_box(site, site_lon, site_lat, box_size = 3, return_limit = False,
+                add_box_name = True, sub_dir = None, override=True):
     import acolite as ac
     import os, json
-
-    region_dir = ac.config['data_dir']+'/Regions'
-    if sub_dir is not None: region_dir =  '{}/{}'.format(region_dir,sub_dir)
-    if not os.path.exists(region_dir): os.makedirs(region_dir)
 
     ## get approximate distance per degree lon/lat
     dlon, dlat = ac.shared.distance_in_ll(site_lat)
@@ -21,19 +18,25 @@ def region_box(site, site_lon, site_lat, box_size = 3, add_box_name = True, sub_
     lon_off = (box_size/dlon)/2
     limit = [site_lat-lat_off, site_lon-lon_off, site_lat+lat_off, site_lon+lon_off]
 
+    if return_limit:
+        return(limit)
+    else:
+        region_dir = ac.config['data_dir']+'/Regions'
+        if sub_dir is not None: region_dir =  '{}/{}'.format(region_dir,sub_dir)
+        if not os.path.exists(region_dir): os.makedirs(region_dir)
 
-    jsonregion = '{}-{}x{}km'.format(site,box_size, box_size) if add_box_name else '{}'.format(site)
+        jsonregion = '{}-{}x{}km'.format(site,box_size, box_size) if add_box_name else '{}'.format(site)
 
-    jsonf = '{}/{}.geojson'.format(region_dir, jsonregion)
-    if (not os.path.exists(jsonf)) or override:
-        ## convert limit to linear ring
-        coordinates = [[[limit[1],limit[0]],
-                        [limit[3],limit[0]],
-                        [limit[3],limit[2]],
-                        [limit[1],limit[2]],
-                        [limit[1],limit[0]] ]]
-        data = {"type":"FeatureCollection",
-                "features":[{"type":"Feature","properties":{},
-                             "geometry":{"type":"Polygon","coordinates":coordinates}}]}
-        with open(jsonf, 'w') as f: json.dump(data, f)
-    return(jsonf)
+        jsonf = '{}/{}.geojson'.format(region_dir, jsonregion)
+        if (not os.path.exists(jsonf)) or override:
+            ## convert limit to linear ring
+            coordinates = [[[limit[1],limit[0]],
+                            [limit[3],limit[0]],
+                            [limit[3],limit[2]],
+                            [limit[1],limit[2]],
+                            [limit[1],limit[0]] ]]
+            data = {"type":"FeatureCollection",
+                    "features":[{"type":"Feature","properties":{},
+                                 "geometry":{"type":"Polygon","coordinates":coordinates}}]}
+            with open(jsonf, 'w') as f: json.dump(data, f)
+        return(jsonf)
