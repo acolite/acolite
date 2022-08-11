5389ea43f88f8110b1fe64b8879d720989a27ab0
acolite <quinten.vanhellemont@naturalsciences.be>
2022-08-11 16:04:25 +0200

    Added wave_range and wave_step

--- acolite/shared/rsr_dict.py
+++ acolite/shared/rsr_dict.py
@@ -4,13 +4,13 @@
 ## 2021-02-27
 ## modifications: 2022-02-25 (QV) added warning printout
 
-def rsr_dict(sensor = None, rsrd = None):
+def rsr_dict(sensor = None, rsrd = None, wave_range=[0.25,2.5],wave_step=0.001):
     import glob, os
     import numpy as np
     import acolite as ac
 
     ## setup wavelength array
-    waves = np.arange(250, 2502.5)/1000
+    waves = np.linspace(wave_range[0],wave_range[1],int(((wave_range[1]-wave_range[0])/wave_step)+1))
 
     ## find rsr files
     if rsrd is None:
@@ -29,7 +29,7 @@ def rsr_dict(sensor = None, rsrd = None):
 
     for fsensor in rsrd:
         ## compute band weighted wavelengths and band names
-        rsrd[fsensor]['wave_mu'] = ac.shared.rsr_convolute_dict(waves, waves, rsrd[fsensor]['rsr'])
+        rsrd[fsensor]['wave_mu'] = ac.shared.rsr_convolute_dict(waves, waves, rsrd[fsensor]['rsr'], wave_range=wave_range,wave_step=wave_step)
         rsrd[fsensor]['wave_nm'] = {b:rsrd[fsensor]['wave_mu'][b]*1000 for b in rsrd[fsensor]['wave_mu']}
         rsrd[fsensor]['wave_name'] = {b:'{:.0f}'.format(rsrd[fsensor]['wave_nm'][b]) for b in rsrd[fsensor]['wave_nm']}
         if 'rsr_bands' not in rsrd[fsensor]:
