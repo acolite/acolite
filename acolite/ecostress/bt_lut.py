e94aa6c81f6dce5dd241f4c32e077dcda5ef1312
acolite <quinten.vanhellemont@naturalsciences.be>
2022-08-11 16:45:16 +0200

    Added download URL

--- acolite/ecostress/bt_lut.py
+++ acolite/ecostress/bt_lut.py
@@ -4,6 +4,7 @@
 ##
 ## written by Quinten Vanhellemont, RBINS
 ## 2022-08-11
+## modifications: 2022-08-11 (QV) added download URL
 
 def bt_lut(bt_lut_file=None):
     import h5py, os
@@ -13,7 +14,9 @@ def bt_lut(bt_lut_file=None):
     ## from https://git.earthdata.nasa.gov/projects/LPDUR/repos/ecostress_swath2grid (accessed 2022-08-11)
     if bt_lut_file is None:
         bt_lut_file = ac.config['data_dir'] + '/ECOSTRESS/EcostressBrightnessTemperatureV01.h5'
-
+        if not os.path.exists(bt_lut_file):
+            url = 'https://git.earthdata.nasa.gov/projects/LPDUR/repos/ecostress_swath2grid/raw/EcostressBrightnessTemperatureV01.h5'
+            ac.shared.download_file(url, bt_lut_file)
     ## read LUT data
     if os.path.exists(bt_lut_file):
         with h5py.File(bt_lut_file, mode='r') as f:
