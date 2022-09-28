775b338e876c104a79e23bdda446e55b18492b7d
acolite <quinten.vanhellemont@naturalsciences.be>
2022-09-28 10:23:43 +0200

    bzipped F0 data

--- acolite/shared/f0_get.py
+++ acolite/shared/f0_get.py
@@ -7,24 +7,37 @@
 ##                2018-07-18 (QV) changed acolite import name
 ##                2018-09-19 (QV) added encoding
 ##                2021-02-05 (QV) changes for acolite-gen
+##                2022-09-28 (QV) added support for bzipped files
 
 def f0_get(f0_file=None, f0_dataset='Thuillier2003'):
     import numpy as np
+    import os,sys,bz2
+    import acolite as ac
 
     if f0_file is None:
-        import os,sys
-        import acolite as ac
-        f0_file = '{}/data/Solar/{}.txt'.format(ac.path, f0_dataset)
+        for ext in ['txt', 'txt.bz2']:
+            f0_file = '{}/data/Solar/{}.{}'.format(ac.path, f0_dataset, ext)
+            if os.path.exists(f0_file):
+                break
 
     f0data=[]
     f0wave=[]
-    with open(f0_file, 'r', encoding='utf-8') as f:
-        for line in f.readlines():
-            line = line.strip()
-            if line[0] in ['#', '!', '/']: continue
-            split = line.split(' ')
-            if len(split) != 2: continue
-            f0data.append(float(split[1]))
-            f0wave.append(float(split[0]))
+    lines = []
+    if os.path.exists(f0_file):
+        p, e = os.path.splitext(f0_file)
+        if e == '.txt':
+            with open(f0_file, 'r', encoding='utf-8') as f:
+                lines = f.readlines()
+        elif e == '.bz2':
+            with bz2.open(f0_file, 'rb') as f:
+                lines = [l.decode('utf-8') for l in f.readlines()]
+
+        for line in lines:
+                line = line.strip()
+                if line[0] in ['#', '!', '/']: continue
+                split = line.split(' ')
+                if len(split) != 2: continue
+                f0data.append(float(split[1]))
+                f0wave.append(float(split[0]))
     f0={"wave":np.asarray(f0wave), "data":np.asarray(f0data)}
     return f0
