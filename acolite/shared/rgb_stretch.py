7161ecfdb99ea59aae5b78e271791c33ffd1a0e2
acolite <quinten.vanhellemont@naturalsciences.be>
2022-12-30 21:50:35 +0100

    Moved gamma stretch

--- acolite/shared/rgb_stretch.py
+++ acolite/shared/rgb_stretch.py
@@ -2,7 +2,7 @@
 ## does stretching for RGB visualisation
 ## written by Quinten Vanhellemont, RBINS
 ## 2022-12-30
-## modifications:
+## modifications: 2022-12-30 (QV) moved gamma stretching
 
 def rgb_stretch(data, gamma=1.0, stretch='linear', bsc=None, percentiles=(5,95)):
     import numpy as np
@@ -17,14 +17,14 @@ def rgb_stretch(data, gamma=1.0, stretch='linear', bsc=None, percentiles=(5,95))
     if bsc is None: bsc = np.nanpercentile(data, percentiles)
 
     ## do stretch
+    data_ = data**gamma
     if stretch == 'linear':
-        tmp = np.interp(data**gamma, bsc, [0, 1])
+        data_ = np.interp(data_, bsc, [0, 1])
     elif stretch == 'log':
         if bsc[0] <= 0: bsc[0] = 0.01
-        tmp = np.interp(np.log(data**gamma), np.log(bsc) , [0, 1])
+        data_ = np.interp(np.log(data_), np.log(bsc) , [0, 1])
     elif stretch == 'sinh':
-        tmp = np.interp(np.sinh(data**gamma), np.sinh(bsc) , [0, 1])
+        data_ = np.interp(np.sinh(data_), np.sinh(bsc) , [0, 1])
     elif stretch == 'sqrt':
-        tmp = np.interp(np.sqrt(data**gamma), np.sqrt(bsc) , [0, 1])
-
-    return(tmp)
+        data_ = np.interp(np.sqrt(data_), np.sqrt(bsc) , [0, 1])
+    return(data_)
