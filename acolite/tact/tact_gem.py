ca8cf34d641f555906f961801e324f3da6f45ce4
acolite <quinten.vanhellemont@naturalsciences.be>
2023-02-02 07:38:22 +0100

    Update number of days

--- acolite/tact/tact_gem.py
+++ acolite/tact/tact_gem.py
@@ -85,7 +85,7 @@ def tact_gem(gem, output_file = True,
             return()
 
     if source == 'era5':
-        max_date = (datetime.datetime.now() - datetime.timedelta(days=90)).isoformat()
+        max_date = (datetime.datetime.now() - datetime.timedelta(days=92)).isoformat()
         if gem['gatts']['isodate'] > max_date:
             print('File too recent for TACT with {} profiles: after {}'.format(source, max_date))
             print('Run with tact_profile_source=gdas1 or tact_profile_source=ncep.reanalysis2 for NRT processing')
