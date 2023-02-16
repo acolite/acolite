Added max_tile_aot, fixed tile masking

acolite/acolite/acolite_l2r.py
@@ -624,12 +624,12 @@ def acolite_l2r(gem,
                                 ## interpolate rho path to observation
                                 aot_band[lut][band_sub] = np.interp(band_data[band_sub], tmp, lutdw[lut]['meta']['tau'], left=np.nan, right=np.nan)
 
-                    tel = time.time()-t0
-
-                    if verbosity > 1: print('{}/B{} {} took {:.3f}s ({})'.format(gem.gatts['sensor'], b, lut, tel, 'RevLUT' if use_revlut else 'StdLUT'))
-
-                ## mask minimum tile aots
-                if setu['dsf_aot_estimate'] == 'tiled': aot_band[lut][aot_band[lut]<setu['dsf_min_tile_aot']]=np.nan
+                    ## mask minimum/maximum tile aots
+                    if setu['dsf_aot_estimate'] == 'tiled':
+                        aot_band[lut][aot_band[lut]<setu['dsf_min_tile_aot']]=np.nan
+                        aot_band[lut][aot_band[lut]>setu['dsf_max_tile_aot']]=np.nan
+                tel = time.time()-t0
+                if verbosity > 1: print('{}/B{} {} took {:.3f}s ({})'.format(gem.gatts['sensor'], b, lut, tel, 'RevLUT' if use_revlut else 'StdLUT'))
 
                 ## store current band results
                 aot_dict[b] = aot_band