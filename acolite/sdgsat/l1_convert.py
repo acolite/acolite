e3571e37a30e6c158cae10e31c006394310cfbff
acolite <quinten.vanhellemont@naturalsciences.be>
2023-02-19 07:37:48 +0100

    Added A/B merging

--- acolite/sdgsat/l1_convert.py
+++ acolite/sdgsat/l1_convert.py
@@ -2,7 +2,8 @@
 ## converts SDGSAT-1 KX10 MII bundle to l1r NetCDF for acolite-gen
 ## written by Quinten Vanhellemont, RBINS
 ## 2023-01-03
-## modifications:
+## modifications: 2023-02-18 (QV) added merging of A and B scenes
+##                2023-02-19 (QV) fixed merged scene nc_projection
 
 def l1_convert(inputfile, output = None, settings = {}, verbosity=5):
     import os
@@ -39,7 +40,7 @@ def l1_convert(inputfile, output = None, settings = {}, verbosity=5):
         ## run through metafiles
         for mi, mf in enumerate(metafiles):
             cf = calfiles[mi]
-            im = imgfiles[mi]
+            #im = imgfiles[mi]
 
             ## read meta and calibration
             meta = ac.sdgsat.metadata(mf)
@@ -122,36 +123,53 @@ def l1_convert(inputfile, output = None, settings = {}, verbosity=5):
             gatts['oname'] = oname
             gatts['ofile'] = ofile
 
+            new = True
+
             ## read image projection
-            dct = ac.shared.projection_read(im)
+            for imi, im in enumerate(imgfiles[mi]):
+                print(imi, im)
+                dct = ac.shared.projection_read(im)
+                if imi == 0:
+                    sub = None
+                    warp_to = None
 
-            sub = None
-            warp_to = None
+                    ## check crop
+                    if (sub is None) & (limit is not None):
+                        dct_sub = ac.shared.projection_sub(dct, limit, four_corners=True)
+                        if dct_sub['out_lon']:
+                            if verbosity > 1: print('Longitude limits outside {}'.format(bundle))
+                            continue
+                        if dct_sub['out_lat']:
+                            if verbosity > 1: print('Latitude limits outside {}'.format(bundle))
+                            continue
+                        sub = dct_sub['sub']
 
-            ## check crop
-            if (sub is None) & (limit is not None):
-                dct_sub = ac.shared.projection_sub(dct, limit, four_corners=True)
-                if dct_sub['out_lon']:
-                    if verbosity > 1: print('Longitude limits outside {}'.format(bundle))
-                    continue
-                if dct_sub['out_lat']:
-                    if verbosity > 1: print('Latitude limits outside {}'.format(bundle))
-                    continue
-                sub = dct_sub['sub']
+                    if sub is None:
+                        dct_prj = {k:dct[k] for k in dct}
+                    else:
+                        gatts['sub'] = sub
+                        gatts['limit'] = limit
+                        ## get the target NetCDF dimensions and dataset offset
+                        if (warp_to is None):
+                            if (setu['extend_region']): ## include part of the roi not covered by the scene
+                                dct_prj = {k:dct_sub['region'][k] for k in dct_sub['region']}
+                            else: ## just include roi that is covered by the scene
+                                dct_prj = {k:dct_sub[k] for k in dct_sub}
+                        ## end cropped
+                else:
+                    ## adapt full image extent
+                    if (imi > 0) & (sub is None):
+                        dct_prj['xrange'] = np.min((dct_prj['xrange'][0], dct['xrange'][0])), \
+                                            np.max((dct_prj['xrange'][1], dct['xrange'][1]))
+                        dct_prj['yrange'] = np.max((dct_prj['yrange'][0], dct['yrange'][0])), \
+                                            np.min((dct_prj['yrange'][1], dct['yrange'][1]))
+                        dct_prj['dimensions'] = np.round((dct_prj['yrange'][1]-dct_prj['yrange'][0]) / dct_prj['pixel_size'][1],0).astype(int), \
+                                                np.round((dct_prj['xrange'][1]-dct_prj['xrange'][0]) / dct_prj['pixel_size'][0],0).astype(int)
+                        dct_prj['ydim'] = dct_prj['dimensions'][0]
+                        dct_prj['xdim'] = dct_prj['dimensions'][1]
+            ## end run through images for projection
 
-            if sub is None:
-                dct_prj = {k:dct[k] for k in dct}
-            else:
-                gatts['sub'] = sub
-                gatts['limit'] = limit
-                ## get the target NetCDF dimensions and dataset offset
-                if (warp_to is None):
-                    if (setu['extend_region']): ## include part of the roi not covered by the scene
-                        dct_prj = {k:dct_sub['region'][k] for k in dct_sub['region']}
-                    else: ## just include roi that is covered by the scene
-                        dct_prj = {k:dct_sub[k] for k in dct_sub}
-                ## end cropped
-
+            ## update gatts
             gatts['scene_xrange'] = dct_prj['xrange']
             gatts['scene_yrange'] = dct_prj['yrange']
             gatts['scene_proj4_string'] = dct_prj['proj4_string']
@@ -181,11 +199,8 @@ def l1_convert(inputfile, output = None, settings = {}, verbosity=5):
             warp_to = (dct_prj['proj4_string'], xyr, dct_prj['pixel_size'][0],dct_prj['pixel_size'][1], res_method)
 
             ## store scene and output dimensions
-            gatts['scene_dims'] = dct['ydim'], dct['xdim']
+            gatts['scene_dims'] = dct_prj['ydim'], dct_prj['xdim']
             gatts['global_dims'] = dct_prj['dimensions']
-
-            ## new file for every bundle if not merging
-            new = True
 
             ## if we are clipping to a given polygon get the clip_mask here
             if clip:
@@ -193,7 +208,7 @@ def l1_convert(inputfile, output = None, settings = {}, verbosity=5):
                 clip_mask = clip_mask.astype(bool) == False
 
             ## write lat/lon
-            if (setu['output_geolocation']):
+            if (setu['output_geolocation']) & (new):
                 if verbosity > 1: print('Writing geolocation lon/lat')
                 lon, lat = ac.shared.projection_geo(dct_prj, add_half_pixel=True)
                 ac.output.nc_write(ofile, 'lon', lon, attributes=gatts, new=new, double=True, nc_projection=nc_projection,
@@ -210,12 +225,12 @@ def l1_convert(inputfile, output = None, settings = {}, verbosity=5):
                 new=False
 
             ## write x/y
-            if (setu['output_xy']):
+            if (setu['output_xy']) & (new):
                 if verbosity > 1: print('Writing geolocation x/y')
                 x, y = ac.shared.projection_geo(dct_prj, xy=True, add_half_pixel=True)
                 ac.output.nc_write(ofile, 'xm', x, new=new,
-                                    netcdf_compression=setu['netcdf_compression'],
-                                    netcdf_compression_level=setu['netcdf_compression_level'])
+                                        netcdf_compression=setu['netcdf_compression'],
+                                        netcdf_compression_level=setu['netcdf_compression_level'])
                 if verbosity > 1: print('Wrote xm ({})'.format(x.shape))
                 x = None
                 ac.output.nc_write(ofile, 'ym', y,
@@ -225,58 +240,61 @@ def l1_convert(inputfile, output = None, settings = {}, verbosity=5):
                 y = None
                 new=False
 
-            ## read bands
-            for i, b in enumerate(cal):
-                bi = int(b)
-                band = rsrd['rsr_bands'][i]
+            ## run through images to load data
+            for imi, im in enumerate(imgfiles[mi]):
+                ## read bands
+                for i, b in enumerate(cal):
+                    bi = int(b)
+                    band = rsrd['rsr_bands'][i]
+                    print('Reading band {} from {}'.format(band, im))
 
-                ## read data and convert to Lt
-                md, data = ac.shared.read_band(im, idx=bi, warp_to=warp_to, gdal_meta=True)
-                nodata = data == np.uint16(0)
-                data = data.astype(float) * cal[b]['gain'] + cal[b]['bias']
+                    ## read data and convert to Lt
+                    md, data = ac.shared.read_band(im, idx=bi, warp_to=warp_to, gdal_meta=True)
+                    nodata = data == np.uint16(0)
+                    data = data.astype(float) * cal[b]['gain'] + cal[b]['bias']
+                    print('Read band {} ({})'.format(band, data.shape))
 
-                ## apply gains
-                if (gains != None) & (setu['gains_parameter'] == 'radiance'):
-                    print('Applying gain {} and offset {} to TOA radiance for band {}'.format(gains[band]['gain'], gains[band]['offset'], band))
-                    data = gains[band]['gain'] * data + gains[band]['offset']
+                    ## apply gains
+                    if (gains != None) & (setu['gains_parameter'] == 'radiance'):
+                        print('Applying gain {} and offset {} to TOA radiance for band {}'.format(gains[band]['gain'], gains[band]['offset'], band))
+                        data = gains[band]['gain'] * data + gains[band]['offset']
 
-                ds_att = {'wavelength':rsrd['wave_nm'][band]}
-                if gains != None:
-                    ds_att['gain'] = gains[band]['gain']
-                    ds_att['offset'] = gains[band]['offset']
-                    ds_att['gains_parameter'] = setu['gains_parameter']
+                    ds_att = {'wavelength':rsrd['wave_nm'][band]}
+                    if gains != None:
+                        ds_att['gain'] = gains[band]['gain']
+                        ds_att['offset'] = gains[band]['offset']
+                        ds_att['gains_parameter'] = setu['gains_parameter']
 
-                if setu['output_lt']:
-                    ds = 'Lt_{}'.format(rsrd['wave_name'][band])
-                    ## write toa radiance
-                    ac.output.nc_write(ofile, ds, data,
-                                  dataset_attributes = ds_att,
-                                  netcdf_compression=setu['netcdf_compression'],
-                                  netcdf_compression_level=setu['netcdf_compression_level'],
-                                  netcdf_compression_least_significant_digit=setu['netcdf_compression_least_significant_digit'])
+                    if setu['output_lt']:
+                        ds = 'Lt_{}'.format(rsrd['wave_name'][band])
+                        ## write toa radiance
+                        ac.output.nc_write(ofile, ds, data,
+                                      dataset_attributes = ds_att,
+                                      netcdf_compression=setu['netcdf_compression'],
+                                      netcdf_compression_level=setu['netcdf_compression_level'],
+                                      netcdf_compression_least_significant_digit=setu['netcdf_compression_least_significant_digit'])
 
-                ## convert to rhot
-                ds = 'rhot_{}'.format(rsrd['wave_name'][band])
-                f0 = gatts['{}_f0'.format(band)]/10
-                data *= (np.pi * gatts['se_distance']**2) / (f0 * np.nanmean(gatts['mus']))
+                    ## convert to rhot
+                    ds = 'rhot_{}'.format(rsrd['wave_name'][band])
+                    f0 = gatts['{}_f0'.format(band)]/10
+                    data *= (np.pi * gatts['se_distance']**2) / (f0 * np.nanmean(gatts['mus']))
 
-                ## apply gains
-                if (gains != None) & (setu['gains_parameter'] == 'reflectance'):
-                    print('Applying gain {} and offset {} to TOA reflectance for band {}'.format(gains[band]['gain'], gains[band]['offset'], band))
-                    data = gains[band]['gain'] * data + gains[band]['offset']
+                    ## apply gains
+                    if (gains != None) & (setu['gains_parameter'] == 'reflectance'):
+                        print('Applying gain {} and offset {} to TOA reflectance for band {}'.format(gains[band]['gain'], gains[band]['offset'], band))
+                        data = gains[band]['gain'] * data + gains[band]['offset']
 
-                data[nodata] = np.nan
-                if clip: data[clip_mask] = np.nan
+                    data[nodata] = np.nan
+                    if clip: data[clip_mask] = np.nan
 
-                ## write to netcdf file
-                ac.output.nc_write(ofile, ds, data, replace_nan=True, attributes=gatts,
-                                        new=new, dataset_attributes = ds_att, nc_projection=nc_projection,
-                                        netcdf_compression=setu['netcdf_compression'],
-                                        netcdf_compression_level=setu['netcdf_compression_level'],
-                                        netcdf_compression_least_significant_digit=setu['netcdf_compression_least_significant_digit'])
-                new = False
-                if verbosity > 1: print('Converting bands: Wrote {} ({})'.format(ds, data.shape))
-
+                    ## write to netcdf file
+                    ac.output.nc_write(ofile, ds, data, replace_nan=True, attributes=gatts,
+                                            new=new, dataset_attributes = ds_att, nc_projection=nc_projection,
+                                            netcdf_compression=setu['netcdf_compression'],
+                                            netcdf_compression_level=setu['netcdf_compression_level'],
+                                            netcdf_compression_least_significant_digit=setu['netcdf_compression_least_significant_digit'])
+                    new = False
+                    if verbosity > 1: print('Converting bands: Wrote {} ({})'.format(ds, data.shape))
 
         if verbosity > 1:
             print('Conversion took {:.1f} seconds'.format(time.time()-t0))
