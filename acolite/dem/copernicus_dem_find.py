960a769e95622af0288a1156ad99d23bd79c76b1
acolite <quinten.vanhellemont@naturalsciences.be>
2023-03-27 15:23:05 +0200

    Fix for negative coordinates

--- acolite/dem/copernicus_dem_find.py
+++ acolite/dem/copernicus_dem_find.py
@@ -13,8 +13,9 @@ def copernicus_dem_find(limit, source = 'copernicus30'):
                  int(math.floor(limit[1])),
                  int(math.ceil(limit[2])),
                  int(math.ceil(limit[3]))]
-    if dem_limit[2] > limit[2]: dem_limit[2]+= -1
-    if dem_limit[3] > limit[3]: dem_limit[3]+= -1
+    if (limit[2] > 0) & (dem_limit[2] > limit[2]): dem_limit[2]+= -1
+    if (limit[3] > 0) & (dem_limit[3] > limit[3]): dem_limit[3]+= -1
+
     ncol = dem_limit[2] - dem_limit[0] + 1
     nrow = dem_limit[3] - dem_limit[1] + 1
 
