6fb85e0709c64c11e64e0d90d430de37f23864d6
acolite <quinten.vanhellemont@naturalsciences.be>
2023-04-26 15:01:24 +0200

    Enable Planet merging without limit

--- acolite/planet/l1_convert.py
+++ acolite/planet/l1_convert.py
@@ -161,14 +161,11 @@ def l1_convert(inputfile, output = None, settings = {},
                     except:
                         print('Failed to import polygon {}'.format(poly))
 
-            ## check if merging settings make sense
-            if (limit is None) & (merge_tiles):
-                if verbosity > 0: print("Merging tiles not supported without ROI limit")
-                merge_tiles = False
             if merge_tiles:
+                if (limit is None):
+                    if verbosity > 0: print("Merging tiles without ROI limit, merging to first tile extent")
                 merge_zones = True
                 extend_region = True
-
         sub = None
 
         ## read rsr
