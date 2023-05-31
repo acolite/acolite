cb306e7ff317429d28d56683c57dafa30be802d8
acolite <quinten.vanhellemont@naturalsciences.be>
2023-05-31 08:39:09 +0200

    Update init

--- acolite/output/__init__.py
+++ acolite/output/__init__.py
@@ -5,3 +5,5 @@ from .nc_write import nc_write
 from .project_acolite_netcdf import project_acolite_netcdf
 from .reproject_acolite_netcdf import reproject_acolite_netcdf
 from .crop_acolite_netcdf import crop_acolite_netcdf
+
+from .convert_l2gen_netcdf import convert_l2gen_netcdf
