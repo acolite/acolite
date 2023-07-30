19c1752c710e39cde7a518a644a4bd63c99196f0
acolite <quinten.vanhellemont@naturalsciences.be>
2023-07-30 16:41:15 +0200

    Added parse keyword

--- acolite/shared/import_config.py
+++ acolite/shared/import_config.py
@@ -1,11 +1,10 @@
 ## simple function to import txt config file
 ## QV 2017-05-24
 ## Last modifications: 2018-07-25 (QV) changed strip and skip characters
+##                     2023-07-30 (QV) added parse keyword
 
-def import_config(file):
+def import_config(file, parse=False):
     import os
-
-
     config={}
     if os.path.exists(file):
         with open(file, 'r', encoding='utf-8') as f:
@@ -16,7 +15,18 @@ def import_config(file):
                 split=line.split('=')
                 if len(split) != 2: continue
                 split = [s.strip() for s in split]
-                config[split[0]]=split[1]
+                if parse:
+                    split[1] = split[1].split(',')
+                    try:
+                        config[split[0]] = [float(s) for s in split[1]]
+                    except:
+                        config[split[0]] = split[1]
+                    if len(config[split[0]]) == 1: config[split[0]]=config[split[0]][0]
+                    if config[split[0]] in ['True','true']: config[split[0]]=True
+                    if config[split[0]] in ['False','false']: config[split[0]]=False
+                    if config[split[0]] in ['None','none']: config[split[0]]=None
+                else:
+                    config[split[0]]=split[1]
     else:
         print('{} not found'.format(file))
     return(config)
