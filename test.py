9b619db8440a19abaebaf603288fe8c5c11a921e
raphidoc <raphael.mabit@gmail.com>
2023-12-21 20:16:11 -0500

    Merge remote-tracking branch 'origin/WISE' into WISE
    # Conflicts:
    #	acolite/wise/__init__.py
    #	acolite/wise/bundle_test.py

--- test.py
+++ test.py
@@ -1,31 +1,33 @@
 import acolite as ac
 import keyring
 
-InFile = '/D/Data/TEST/TEST_WISE/MC-10/190820_MC-10A-WI-1x1x1_v02-L1G.pix'
+#InFile = '/D/Data/TEST/TEST_WISE/MC-10/190820_MC-10A-WI-1x1x1_v02-L1G.pix'
+# After L1R conversion
+InFile = '/D/Data/TEST/TEST_WISE/MC-10/L2_ACOLITE/wise_2606_190820_MC-10A-WI-1x1x1_v02_2019_08_20_15_11_38_L1R.nc'
 OutPath = '/D/Data/TEST/TEST_WISE/MC-10/L2_ACOLITE'
 
 passwd = keyring.get_password("EarthData", "raphidoc")
 
 Settings = {"inputfile":InFile,
             "output":OutPath,
-            "polygon_limit":True,
+            "blackfill_skip":True,
+            "blackfill_wave":802,
             'aerosol_correction':'dark_spectrum',
-            "dsf_residual_glint_correction": True,
             'dsf_write_aot_550':True,
             'dsf_write_tiled_parameters':False,
+            "dsf_residual_glint_correction": False,
             'glint_write_rhog_ref':False,
             "l2w_parameters":['rhow_*'],
-            #"output_xy":True,
+            "l2w_mask_high_toa":False,
             "output_geolocation":True,
-            "output_xy":False,
-            "reproject_outputs":'L2w',
+            "output_xy":True,
             "output_projection":False,
-            "reproject_before_ac":False,
-            "output_projection_epsg":4326,
             'netcdf_projection':True,
             "ancillary_data":True,
             "EARTHDATA_u":"raphidoc",
-            "EARTHDATA_p":passwd
+            "EARTHDATA_p":passwd,
+            "output_lt":False,
+            "l2w_mask_negative_rhow":False
             }
 
 ac.acolite.acolite_run(settings=Settings)
