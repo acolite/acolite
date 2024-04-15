0a0403c86b3d396a3b864cd7e198d0c229b7c047
acolite <quinten.vanhellemont@naturalsciences.be>
2024-04-15 09:56:29 +0200

    Update nc reading

--- acolite/shared/nc_read.py
+++ acolite/shared/nc_read.py
@@ -18,7 +18,10 @@ def nc_read(file, dataset, group = None):
 # read dataset from netcdf
 # Last updates: 2016-12-19 (QV) added crop (x0,x1,y0,y1)
 ##              2017-03-16 (QV) added sub keyword (xoff, yoff, xcount, ycount)
-def nc_data(file, dataset, crop=False, sub=None, attributes=False, group=None):
+##              2023-10-31 (QV) added dtype keyword
+##              2024-02-21 (QV) assume 3d array subset is keeps the first dimension
+
+def nc_data(file, dataset, crop=False, sub=None, attributes=False, group=None, dtype=None):
     from netCDF4 import Dataset
     with Dataset(file) as nc:
         if group is not None:
@@ -27,13 +30,23 @@ def nc_data(file, dataset, crop=False, sub=None, attributes=False, group=None):
             if crop is False:
                 data = nc.variables[dataset][:]
             else:
-                if len(crop) == 4: data = nc.variables[dataset][crop[2]:crop[3]:1,crop[0]:crop[1]:1]
+                if len(crop) == 4:
+                    if len(nc.variables[dataset].shape) == 3:
+                        data = nc.variables[dataset][:, crop[2]:crop[3]:1,crop[0]:crop[1]:1]
+                    else:
+                        data = nc.variables[dataset][crop[2]:crop[3]:1,crop[0]:crop[1]:1]
                 else: data = nc.variables[dataset][:]
         else:
-            if len(sub) == 4: data = nc.variables[dataset][sub[1]:sub[1]+sub[3]:1,sub[0]:sub[0]+sub[2]:1]
+            if len(sub) == 4:
+                if len(nc.variables[dataset].shape) == 3:
+                    data = nc.variables[dataset][:, sub[1]:sub[1]+sub[3]:1,sub[0]:sub[0]+sub[2]:1]
+                else:
+                    data = nc.variables[dataset][sub[1]:sub[1]+sub[3]:1,sub[0]:sub[0]+sub[2]:1]
             else: data = nc.variables[dataset][:]
         if attributes:
             atts = {attr : getattr(nc.variables[dataset],attr) for attr in nc.variables[dataset].ncattrs()}
+    if dtype is not None: data = data.astype(dtype)
+
     if attributes:
         return(data,atts)
     else:
@@ -54,6 +67,18 @@ def nc_gatts(file):
     with Dataset(file) as nc:
         gatts = {attr : getattr(nc,attr) for attr in nc.ncattrs()}
     return gatts
+
+# read groups from netcdf
+def nc_groups(file, datasets = True):
+    from netCDF4 import Dataset
+    with Dataset(file) as nc:
+        if datasets:
+            groups = {}
+            for g in nc.groups:
+                groups[g] = [v for v in nc.groups[g].variables]
+        else:
+            groups = [g for g in nc.groups]
+    return groups
 
 # read datasets in netcdf
 # Last updates: 2023-05-31 (QV) added group keyword
