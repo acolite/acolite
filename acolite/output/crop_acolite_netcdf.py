e22d5660b930039ff3cab091b478f63b2d3dc6f0
acolite <quinten.vanhellemont@naturalsciences.be>
2024-04-16 13:32:24 +0200

    Use gem

--- acolite/output/crop_acolite_netcdf.py
+++ acolite/output/crop_acolite_netcdf.py
@@ -3,6 +3,7 @@
 ## written by Quinten Vanhellemont, RBINS
 ## 2023-04-02
 ## modifications: 2023-04-09 (QV) Fix for NetCDF without nc_projection
+##                2024-04-16 (QV) Use gem for input/output
 
 def crop_acolite_netcdf(ncf, output=None, limit=None, polygon=None):
     import os
@@ -15,42 +16,46 @@ def crop_acolite_netcdf(ncf, output=None, limit=None, polygon=None):
         print("Please provide a four element limit [S, W, N, E] or a polygon file for L1R cropping")
         return
 
-    gatts = ac.shared.nc_gatts(ncf)
+    gem = ac.gem.gem(ncf)
     dn = os.path.dirname(ncf)
     bn = os.path.basename(ncf)
     if output is None: output = dn
 
-    datasets = ac.shared.nc_datasets(ncf)
-    nc_projection = ac.shared.nc_read_projection(ncf)
-
-    ofile = '{}/{}'.format(dn, bn.replace('.nc', '_crop.nc'))
+    ofile = '{}/{}'.format(output, bn.replace('.nc', '_crop.nc'))
 
     ## load lat/lon and crop
-    lat, latatt = ac.shared.nc_data(ncf, 'lat', attributes=True)
-    lon, lonatt = ac.shared.nc_data(ncf, 'lon', attributes=True)
+    lat, latatt = gem.data('lat', attributes = True)
+    lon, lonatt = gem.data('lon', attributes = True)
     sub = ac.shared.geolocation_sub(lat, lon, limit)
 
     if sub is not None:
         print('Cropping file {} to {}'.format(ncf,ofile))
         print('New limit {}'.format(limit))
         print('Sub {}'.format(sub))
-        if nc_projection is not None:
-            nc_projection['x']['data'] = nc_projection['x']['data'][sub[0]:sub[0]+sub[2]]
-            nc_projection['y']['data'] = nc_projection['y']['data'][sub[1]:sub[1]+sub[3]]
+
+        ## setup output dataset
+        gemo = ac.gem.gem(ofile, new = True)
+        gemo.gatts = {k: gem.gatts[k] for k in gem.gatts} ## set output attributes
+        gemo.gatts['ofile'] = ofile
+        gemo.verbosity = 6
+
+        if gem.nc_projection is not None:
+            gemo.nc_projection = gem.nc_projection
+            gemo.nc_projection['x']['data'] = gemo.nc_projection['x']['data'][sub[0]:sub[0]+sub[2]]
+            gemo.nc_projection['y']['data'] = gemo.nc_projection['y']['data'][sub[1]:sub[1]+sub[3]]
 
         new = True
-        for ds in datasets:
-            if nc_projection is not None:
-                if ds in nc_projection: continue
+        for ds in gem.datasets:
+            if gem.nc_projection is not None:
+                if ds in gem.nc_projection_keys: continue
 
             try:
-                d, da = ac.shared.nc_data(ncf, ds, attributes=True, sub=sub)
+                d, da = gem.data(ds, attributes = True, sub = sub)
             except:
                 print('Could not crop dataset {}'.format(ds))
                 continue
+
             ## output dataset
-            ac.output.nc_write(ofile, ds, d, dataset_attributes = da,
-                               new=new, attributes=gatts, nc_projection=nc_projection)
+            gemo.write(ds, d, ds_att = da)
             print('Wrote {} ({}) to {}'.format(ds, d.shape, ofile))
-            new = False
         return(ofile)
