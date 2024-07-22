6e3b589a45bc83c3079154c4b3b7772ad75350d1
acolite <quinten.vanhellemont@naturalsciences.be>
2024-07-22 13:47:17 +0200

    Updated hyper RSR

--- acolite/shared/gauss_response.py
+++ acolite/shared/gauss_response.py
@@ -3,10 +3,11 @@
 ## written by Quinten Vanhellemont, RBINS for the PONDER project
 ## 2018-01-30
 ## modifications: 2021-06-07 (QV) changed numpy import added to generic acolite
+##                2024-07-03 (QV) added factor keyword
 
-def gauss_response(center, fwhm, step=1):
+def gauss_response(center, fwhm, step = 1, factor = 1.5):
     import numpy as np
-    wrange = (center - 1.5*fwhm, center + 1.5*fwhm)
+    wrange = (center - factor*fwhm, center + factor*fwhm)
     sigma = fwhm / (2*np.sqrt(2*np.log(2)))
     x = np.linspace(wrange[0], wrange[1], int(1+(wrange[1]-wrange[0])/step))
     y = np.exp(-((x-center)/sigma)**2 )
--- acolite/shared/rsr_hyper.py
+++ acolite/shared/rsr_hyper.py
@@ -2,14 +2,20 @@
 ## makes hyperspectral RSR based on center wavelength and band width
 ## written by Quinten Vanhellemont, RBINS
 ## 2021-06-08
+## modifications: 2024-07-03 (QV) added type and factor keywords
 
-def rsr_hyper(waves, widths, step = 0.25, nm = True):
+def rsr_hyper(waves, widths, step = 0.25, nm = True, type = 'gauss', factor = None):
     import acolite as ac
 
     nbands = len(waves)
     rsr = {}
     for b in range(nbands):
-        wave, resp = ac.shared.gauss_response(waves[b], widths[b], step=step)
+        if type == 'gauss':
+            if factor is None: factor = 1.5
+            wave, resp = ac.shared.gauss_response(waves[b], widths[b], step = step, factor = factor)
+        elif type == 'square':
+            if factor is None: factor = 0.5
+            wave, resp = ac.shared.square_response(waves[b], widths[b], step = step, factor = factor)
         rsr[b] = {'wave':wave, 'response': resp}
         if nm: rsr[b]['wave']/=1000
     return(rsr)
