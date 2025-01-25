1d4cccb339e4f81da3fe3f577f7c0053340d92b0
acolite <quinten.vanhellemont@naturalsciences.be>
2025-01-25 09:49:59 +0100

    Allow passing of open gdal.Dataset

--- acolite/shared/projection_read.py
+++ acolite/shared/projection_read.py
@@ -5,18 +5,29 @@
 ## modifications: 2022-08-06 (QV) added Wkt, set up Proj from Wkt
 ##                2022-12-13 (QV) use transform from world file if available
 ##                2024-03-28 (QV) x/y range and pixel_size as lists
+##                2025-01-24 (QV) allow passing of Open gdal dataset
 
 def projection_read(file):
     from pyproj import Proj
     from osgeo import gdal,osr
     import os
 
-    ## open file
-    ds = gdal.Open(file)
+    gdal.UseExceptions()
+
+    close = False
+    if type(file) == str:
+        ds = gdal.Open(file)
+        close = True ## close if we open file here
+    elif type(file) == gdal.Dataset:
+        ds = file
+    else:
+        print('{} not recognised'.format(file))
+        return()
+
     transform = ds.GetGeoTransform()
     projection = ds.GetProjection()
     dimx, dimy = ds.RasterXSize, ds.RasterYSize
-    ds = None
+    if close: ds = None
 
     ## get projection info
     src = osr.SpatialReference()
@@ -25,24 +36,24 @@ def projection_read(file):
     p = Proj(Wkt)
 
     ## find world file if present
-    for ext in ['J2W', 'TFW', 'WLD', 'j2w', 'tfw', 'wld']:
-        fbase, fext = os.path.splitext(file)
-        wfile = '{}.{}'.format(fbase, ext)
-        if os.path.exists(wfile):
-            break
-        wfile = None
-    ## read world file
-    if wfile is not None:
-        wtransform = []
-        with open(wfile, 'r') as f:
-            for l in f.readlines():
-                wtransform.append(float(l.strip()))
-        if len(wtransform) != 6:
-            print('World file {} has wrong number of elements'.format(wfile))
-            wtransform = None
-    else:
-        wtransform = None
-
+    wtransform = None
+    if type(file) == str:
+        for ext in ['J2W', 'TFW', 'WLD', 'j2w', 'tfw', 'wld']:
+            fbase, fext = os.path.splitext(file)
+            wfile = '{}.{}'.format(fbase, ext)
+            if os.path.exists(wfile):
+                break
+            wfile = None
+        ## read world file
+        if wfile is not None:
+            wtransform = []
+            with open(wfile, 'r') as f:
+                for l in f.readlines():
+                    wtransform.append(float(l.strip()))
+            if len(wtransform) != 6:
+                print('World file {} has wrong number of elements'.format(wfile))
+                wtransform = None
+        
     if wtransform is not None:
         ## world transform elements:
         ## 0 pixel X size
