0b2946c8c6eb6e74a31b773774b49a54b32b36f5
acolite <quinten.vanhellemont@naturalsciences.be>
2025-01-30 23:34:28 +0100

    Changed to nansum

--- acolite/shared/rsr_convolute_dict.py
+++ acolite/shared/rsr_convolute_dict.py
@@ -5,6 +5,7 @@
 ## modifications: 2017-10-17 (QV) moved wave range and step to keywords, renamed from interprsr
 ##                2020-03-02 (QV) added ceil to number of elements in linspace
 ##                2022-10-25 (QV) changed upper bound of default range to 2.5
+##                2024-11-04 (QV) changed to np.nansum
 
 def rsr_convolute_dict(wave_data, data, rsr, wave_range=[0.2,2.55], wave_step=0.001):
     import numpy as np
@@ -17,11 +18,11 @@ def rsr_convolute_dict(wave_data, data, rsr, wave_range=[0.2,2.55], wave_step=0.
     for band in rsr:
         band_wave_hyper = wave_hyper
         band_response_hyper = np.interp(wave_hyper, rsr[band]['wave'], rsr[band]['response'], left=0, right=0)
-        band_response_sum = sum(band_response_hyper)
+        band_response_sum = np.nansum(band_response_hyper)
         rsr_hyper[band]={'wave':band_wave_hyper, 'response': band_response_hyper, 'sum':band_response_sum}
 
     resdata={}
     data_hyper = np.interp(wave_hyper, wave_data, data, left=0, right=0)
     for band in rsr:
-        resdata[band] = (sum(data_hyper*rsr_hyper[band]['response'])/rsr_hyper[band]['sum'])
+        resdata[band] = (np.nansum(data_hyper*rsr_hyper[band]['response'])/rsr_hyper[band]['sum'])
     return resdata
