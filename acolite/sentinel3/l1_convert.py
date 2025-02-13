cfc3693703b861dfc9670426c1369a07037aca3b
acolite <quinten.vanhellemont@naturalsciences.be>
2025-02-13 16:26:40 +0100

    Changed to settings.merge

--- acolite/sentinel3/l1_convert.py
+++ acolite/sentinel3/l1_convert.py
@@ -29,14 +29,9 @@ def l1_convert(inputfile, output = None, settings = None, convert_l2 = False, wr
     import scipy.interpolate
     import acolite as ac
 
-    ## get run settings
-    setu = {k: ac.settings['run'][k] for k in ac.settings['run']}
-
-    ## additional run settings
-    if settings is not None:
-        settings = ac.acolite.settings.parse(settings)
-        for k in settings: setu[k] = settings[k]
-    ## end additional run settings
+    ## get run/user/sensor settings
+    setu = ac.acolite.settings.merge(sensor = None, settings = settings)
+    verbosity = setu['verbosity']
 
     ## parse inputfile
     if type(inputfile) != list:
@@ -47,7 +42,7 @@ def l1_convert(inputfile, output = None, settings = None, convert_l2 = False, wr
     nscenes = len(inputfile)
     if setu['verbosity'] > 1: print('Starting conversion of {} scene{}'.format(nscenes, '' if nscenes==1 else 's'))
 
-    ofile = None
+    ## list to store output files
     ofiles = []
 
     ## test if we need to merge
@@ -104,12 +99,8 @@ def l1_convert(inputfile, output = None, settings = None, convert_l2 = False, wr
         rsrd = ac.shared.rsr_dict(sensor)
         rsr_bands = rsrd[sensor]['rsr_bands']
 
-        ## get sensor specific defaults
-        setd = ac.acolite.settings.parse(sensor)
-        ## set sensor default if user has not specified the setting
-        for k in setd:
-            if k not in ac.settings['user']: setu[k] = setd[k]
-        ## end set sensor specific defaults
+        ## update settings
+        setu = ac.acolite.settings.merge(sensor = sensor, settings = settings)
 
         if output is None: output = setu['output']
 
