7d259d3ed00d0eaa9b21babed47cab2d5c73c47c
acolite <quinten.vanhellemont@naturalsciences.be>
2025-04-03 06:58:23 +0200

    Added run settings

--- acolite/shared/auth.py
+++ acolite/shared/auth.py
@@ -2,7 +2,7 @@
 ## gets authentication credentials
 ## written by Quinten Vanhellemont, RBINS
 ## 2023-09-25
-## modifications:
+## modifications: 2025-04-03 (QV) added run settings
 
 def auth(machine):
     import os, requests, json, netrc
@@ -25,16 +25,29 @@ def auth(machine):
     if auth is None:
         if ('{}_u'.format(machine.upper()) in os.environ) & \
            ('{}_p'.format(machine.upper()) in os.environ):
-            auth = (os.environ['{}_u'.format(machine.upper())], os.environ['{}_p'.format(machine.upper())])
+            auth = (os.environ['{}_u'.format(machine.upper())], \
+                    os.environ['{}_p'.format(machine.upper())])
 
     ## get auth from config
     if auth is None:
-        if (ac.config['{}_u'.format(machine.upper())] != '') & \
-           (ac.config['{}_p'.format(machine.upper())] != ''):
-            auth = (ac.config['{}_u'.format(machine.upper())], ac.config['{}_p'.format(machine.upper())])
+        if ('{}_u'.format(machine.upper()) in ac.config) & \
+           ('{}_p'.format(machine.upper()) in ac.config):
+            if (ac.config['{}_u'.format(machine.upper())] != '') & \
+               (ac.config['{}_p'.format(machine.upper())] != ''):
+                auth = (ac.config['{}_u'.format(machine.upper())], \
+                        ac.config['{}_p'.format(machine.upper())])
+
+    ## get auth from run settings
+    if auth is None:
+        if ('{}_u'.format(machine.upper()) in ac.settings['run']) & \
+           ('{}_p'.format(machine.upper()) in ac.settings['run']):
+            if (ac.settings['run']['{}_u'.format(machine.upper())] != '') & \
+               (ac.settings['run']['{}_p'.format(machine.upper())] != ''):
+                auth = (ac.settings['run']['{}_u'.format(machine.upper())], \
+                        ac.settings['run']['{}_p'.format(machine.upper())])
 
     if auth is None:
-        print('Could not determine {} credentials. Please add them to your .netrc file or ACOLITE config file.'.format(machine))
+        print('Could not determine {} credentials. Please add them to your .netrc file or ACOLITE credentials file.'.format(machine))
         return
 
     return(auth)
