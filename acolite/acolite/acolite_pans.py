49667edc2394e0c8ced3a52b19b795442217e0a2
acolite <quinten.vanhellemont@naturalsciences.be>
2025-04-10 09:28:21 +0200

    Updated settings handling

--- acolite/acolite/acolite_pans.py
+++ acolite/acolite/acolite_pans.py
@@ -6,6 +6,7 @@
 ##                2024-03-14 (QV) update settings handling
 ##                                removed RGB/geotiff outputs
 ##                2024-04-16 (QV) use new gem NetCDF handling
+##                2025-04-10 (QV) updated settings handling
 
 def acolite_pans(ncf, output = None, settings = None):
 
@@ -31,12 +32,21 @@ def acolite_pans(ncf, output = None, settings = None):
     ## Open gem
     gem = ac.gem.gem(ncf)
 
-    ## parse settings
-    if 'user' not in ac.settings:
-        ac.settings['user'] = ac.acolite.settings.parse(None, settings=settings, merge=False)
-        for k in ac.settings['user']: ac.settings['run'][k] = ac.settings['user'][k]
-    setu = ac.acolite.settings.parse(gem.gatts['sensor'], settings=settings)
-    for k in ac.settings['user']: setu[k] = ac.settings['user'][k]
+    ## combine default and user defined settings
+    ## get run settings
+    setu = {k: ac.settings['run'][k] for k in ac.settings['run']}
+    ## get sensor specific defaults
+    setd = ac.acolite.settings.parse(gem.gatts['sensor'])
+    ## set sensor default if user has not specified the setting
+    for k in setd:
+        if k not in ac.settings['user']: setu[k] = setd[k]
+    ## end set sensor specific defaults
+
+    ## additional run settings
+    if settings is not None:
+        settings = ac.acolite.settings.parse(settings)
+        for k in settings: setu[k] = settings[k]
+    ## end additional run settings
 
     if setu['pans_method'] not in ['panr', 'visr']:
         print('pans_method={} not configured, using panr')
@@ -87,7 +97,7 @@ def acolite_pans(ncf, output = None, settings = None):
     ## read pan data
     pan = gemp.data(dsp)
     gemp.close()
-    print(pan.shape)
+    print(dsp, pan.shape)
 
     print('Using pans_method={}'.format(setu['pans_method']))
     ## compute pan factor
