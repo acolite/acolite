2a7247a6569dccff81e6b191719f3a5341513d3d
acolite <quinten.vanhellemont@naturalsciences.be>
2025-04-14 16:05:06 +0200

    Removed format error

--- acolite/acolite/acolite_map.py
+++ acolite/acolite/acolite_map.py
@@ -549,7 +549,7 @@ def acolite_map(ncf, output = None,
     if setu['map_scalebar']:
         if setu['map_scalebar_position'] not in ['UR','UL','LL','LR']:
             print('Map scalebar position {} not recognised.')
-            print('Using default map_scalebar_position=UL.'.format(scalepos))
+            print('Using default map_scalebar_position=UL.')
             setu['map_scalebar_position'] = 'UL'
         posv = {'U': 0.85, 'L': 0.10}
         posh = {'R': 0.95, 'L': 0.05}
