461b56282faca3e9c41a9192a7e81a6331a79685
acolite <quinten.vanhellemont@naturalsciences.be>
2025-07-09 13:50:35 +0200

    Changed token handling

--- acolite/api/earthexplorer/auth.py
+++ acolite/api/earthexplorer/auth.py
@@ -5,9 +5,10 @@
 ## modifications: 2023-09-25 (QV) changed to separate earthexplorer credentials
 ##                2024-04-27 (QV) moved to acolite.api
 ##                2025-04-03 (QV) use login-token
+##                2025-07-09 (QV) update code to get token from ac.shared.auth
 
 def auth(api_url = None, return_auth = False, netrc_machine = 'earthexplorer',
-         user = None, password = None, token = None):
+         user = None, token = None):
     import os, requests, json, netrc
     import acolite as ac
 
@@ -15,62 +16,18 @@ def auth(api_url = None, return_auth = False, netrc_machine = 'earthexplorer',
     auth = ac.shared.auth(netrc_machine)
 
     if auth is None:
-        print('Could not determine {} credentials {}_u and {}_p.'.format(netrc_machine.upper(), netrc_machine.upper(), netrc_machine.upper()))
+        print('Could not determine {} credentials {}_u and {}_token.'.format(netrc_machine.upper(), netrc_machine.upper(), netrc_machine.upper()))
         print('Please add them to your .netrc file, environment variables, or ACOLITE config file.')
 
-    ## get username
-    # user_key = 'EARTHEXPLORER_u'
-    # if (user is None) & (user_key in ac.settings['run']):
-    #     if (ac.settings['run'][user_key] not in [None, '']):
-    #         user = '{}'.format(ac.settings['run'][user_key])
-    # if (user is None) & (user_key in ac.config):
-    #     if (ac.config[user_key] not in [None, '']):
-    #         user = '{}'.format(ac.config[user_key])
-    # if (user is None) & (user_key in os.environ):
-    #     if (os.environ[user_key] not in [None, '']):
-    #         user = '{}'.format(os.environ[user_key])
-    # if (user is None):
-    #     print('Could not determine {}.'.format(user_key))
-    #     print('Please provide an {} as environment variable, or in the settings or credentials file.'.format(user_key))
-    #     return()
-
-    ## get password
-    # pass_key = 'EARTHEXPLORER_p'
-    # if (password is None) & (pass_key in ac.settings['run']):
-    #     if (ac.settings['run'][pass_key] not in [None, '']):
-    #         password = '{}'.format(ac.settings['run'][pass_key])
-    # if (password is None) & (pass_key in ac.config):
-    #     if (ac.config[pass_key] not in [None, '']):
-    #         password = '{}'.format(ac.config[pass_key])
-    # if (password is None) & (pass_key in os.environ):
-    #     if (os.environ[pass_key] not in [None, '']):
-    #         password = '{}'.format(os.environ[pass_key])
-    # if (password is None):
-    #     print('Could not determine {}.'.format(pass_key))
-    #     print('Please provide an {} as environment variable, or in the settings or credentials file.'.format(pass_key))
-    #     return()
-
-    ## get token
-    token_key = 'EARTHEXPLORER_token'
-    if (token is None) & (token_key in ac.settings['run']):
-        if (ac.settings['run'][token_key] not in [None, '']):
-            token = '{}'.format(ac.settings['run'][token_key])
-    if (token is None) & (token_key in ac.config):
-        if (ac.config[token_key] not in [None, '']):
-            token = '{}'.format(ac.config[token_key])
-    if (token is None) & (token_key in os.environ):
-        if (os.environ[token_key] not in [None, '']):
-            token = '{}'.format(os.environ[token_key])
-    if (token is None):
-        print('Could not determine {}.'.format(token_key))
-        print('Please provide an {} as environment variable, or in your settings or the credentials file.'.format(token_key))
-        return()
+    if (len(auth[1]) != 64):
+        print('{}_token length is not 64 characters, this is likely wrong.'.format(netrc_machine.upper()))
+        print('Retrieving access_token is unlikely to work!')
 
     ## get api URL from config
     if api_url is None: api_url = ac.config['EARTHEXPLORER_api']
 
     ## get access token
-    data = {"username": auth[0], "token": token}
+    data = {"username": auth[0], "token": auth[1]}
     response = requests.post(api_url+'/login-token', data = json.dumps(data))
     access_token = response.json()["data"]
 
--- acolite/shared/auth.py
+++ acolite/shared/auth.py
@@ -3,6 +3,7 @@
 ## written by Quinten Vanhellemont, RBINS
 ## 2023-09-25
 ## modifications: 2025-04-03 (QV) added run settings
+##                2025-07-09 (QV) added token option
 
 def auth(machine):
     import os, requests, json, netrc
@@ -23,28 +24,41 @@ def auth(machine):
 
     ## get auth from environment
     if auth is None:
-        if ('{}_u'.format(machine.upper()) in os.environ) & \
-           ('{}_p'.format(machine.upper()) in os.environ):
-            auth = (os.environ['{}_u'.format(machine.upper())], \
-                    os.environ['{}_p'.format(machine.upper())])
+        if ('{}_u'.format(machine.upper()) in os.environ):
+            if ('{}_p'.format(machine.upper()) in os.environ):
+                auth = (os.environ['{}_u'.format(machine.upper())], \
+                        os.environ['{}_p'.format(machine.upper())])
+            if ('{}_token'.format(machine.upper()) in os.environ):
+                auth = (os.environ['{}_u'.format(machine.upper())], \
+                        os.environ['{}_token'.format(machine.upper())])
 
     ## get auth from config
     if auth is None:
-        if ('{}_u'.format(machine.upper()) in ac.config) & \
-           ('{}_p'.format(machine.upper()) in ac.config):
-            if (ac.config['{}_u'.format(machine.upper())] != '') & \
-               (ac.config['{}_p'.format(machine.upper())] != ''):
-                auth = (ac.config['{}_u'.format(machine.upper())], \
-                        ac.config['{}_p'.format(machine.upper())])
+        if ('{}_u'.format(machine.upper()) in ac.config):
+            if ('{}_p'.format(machine.upper()) in ac.config):
+                if (ac.config['{}_u'.format(machine.upper())] != '') & \
+                   (ac.config['{}_p'.format(machine.upper())] != ''):
+                    auth = (ac.config['{}_u'.format(machine.upper())], \
+                            ac.config['{}_p'.format(machine.upper())])
+            if ('{}_token'.format(machine.upper()) in ac.config):
+                if (ac.config['{}_u'.format(machine.upper())] != '') & \
+                   (ac.config['{}_token'.format(machine.upper())] != ''):
+                    auth = (ac.config['{}_u'.format(machine.upper())], \
+                            ac.config['{}_token'.format(machine.upper())])
 
     ## get auth from run settings
     if auth is None:
-        if ('{}_u'.format(machine.upper()) in ac.settings['run']) & \
-           ('{}_p'.format(machine.upper()) in ac.settings['run']):
-            if (ac.settings['run']['{}_u'.format(machine.upper())] != '') & \
-               (ac.settings['run']['{}_p'.format(machine.upper())] != ''):
-                auth = (ac.settings['run']['{}_u'.format(machine.upper())], \
-                        ac.settings['run']['{}_p'.format(machine.upper())])
+        if ('{}_u'.format(machine.upper()) in ac.settings['run']):
+            if ('{}_p'.format(machine.upper()) in ac.settings['run']):
+                if (ac.settings['run']['{}_u'.format(machine.upper())] != '') & \
+                   (ac.settings['run']['{}_p'.format(machine.upper())] != ''):
+                    auth = (ac.settings['run']['{}_u'.format(machine.upper())], \
+                            ac.settings['run']['{}_p'.format(machine.upper())])
+            if ('{}_token'.format(machine.upper()) in ac.settings['run']):
+                if (ac.settings['run']['{}_u'.format(machine.upper())] != '') & \
+                   (ac.settings['run']['{}_token'.format(machine.upper())] != ''):
+                    auth = (ac.settings['run']['{}_u'.format(machine.upper())], \
+                            ac.settings['run']['{}_token'.format(machine.upper())])
 
     if auth is None:
         print('Could not determine {} credentials. Please add them to your .netrc file or ACOLITE credentials file.'.format(machine))
