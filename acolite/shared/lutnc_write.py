5ebb697e8d6661081d4660eb16dfbfea74d1f3c3
acolite <quinten.vanhellemont@naturalsciences.be>
2025-10-30 08:44:17 +0100

    Added directory check

--- acolite/shared/lutnc_write.py
+++ acolite/shared/lutnc_write.py
@@ -3,7 +3,7 @@
 ##
 ## written by Quinten Vanhellemont, RBINS
 ## 2022-03-23
-## modifications:
+## modifications: 2025-10-30 (QV) added directory check
 
 def lutnc_write(lutnc, lut, meta_in, dims = None, override = True, compression = True, complevel = 1):
     import os
@@ -13,6 +13,10 @@ def lutnc_write(lutnc, lut, meta_in, dims = None, override = True, compression =
 
     if os.path.exists(lutnc) & override:
         os.remove(lutnc)
+
+    ## create output directory
+    if not os.path.exists(os.path.dirname(lutnc)):
+        os.makedirs(os.path.dirname(lutnc))
 
     ## update dims
     meta = {k: meta_in[k] for k in meta_in}
