35054161ef7929681513b9ebf15c360af05c3865
acolite <quinten.vanhellemont@naturalsciences.be>
2025-11-19 15:36:45 +0100

    Added NetCDF test

--- acolite/shared/download_file.py
+++ acolite/shared/download_file.py
@@ -12,6 +12,7 @@
 ##                2024-05-01 (QV) added earthdatacloud.nasa.gov check for earthdata
 ##                2024-05-22 (QV) use EARTHDATA_urls from config
 ##                2025-07-09 (QV) use ac.shared.auth for earthdata
+##                2025-11-19 (QV) added test of downloaded NetCDF files
 
 def download_file(url, file, auth = None, session = None,
                     parallel = False, verbosity = 0, verify_ssl = True, retry = 1):
@@ -36,28 +37,43 @@ def download_file(url, file, auth = None, session = None,
             print('EARTHDATA user name and password required for download of {}'.format(url))
             return()
 
+    retry_download = False
     with requests.Session() as session:
             r1 = session.request('get', url, verify=verify_ssl)
             r = session.get(r1.url, auth=auth, verify=verify_ssl)
             time.sleep(1)
 
             if (r.ok):
+                ## download to temp file
                 with open(temp_file, 'wb') as f:
                     for chunk in r.iter_content(chunk_size=1024*1024):
                         if chunk: # filter out keep-alive new chunks
                             f.write(chunk)
-            else:
-                if verbosity > 2: print(r.text)
-                if retry > 0:
-                    retry -= 1
-                    print('Retrying...')
-                    ac.shared.download_file(url, file, auth = auth, session = session,
-                                                parallel = parallel, verbosity = verbosity, verify_ssl = verify_ssl, retry = retry)
-                if not os.path.exists(file):
-                    raise Exception("File download failed {}".format(r.text))
 
-    ## copy temp file
-    if os.path.exists(temp_file):
+                ## test netcdf file
+                if temp_file.endswith('.nc') | temp_file.endswith('.hdf'):
+                    try:
+                        gatts = ac.shared.nc_gatts(temp_file)
+                    except:
+                        print('Could not read NetCDF gatts for {}'.format(temp_file))
+                        retry_download = True
+            else:
+                retry_download = True
+                if verbosity > 2: print(r.text)
+
+    ## retry download if it failed
+    if retry_download:
+        if os.path.exists(temp_file): os.remove(temp_file)
+        if (retry > 0):
+            retry -= 1
+            print('Retrying...')
+            ac.shared.download_file(url, file, auth = auth, session = session,
+                            parallel = parallel, verbosity = verbosity, verify_ssl = verify_ssl, retry = retry)
+
+    ## move file if it exists, otherwise raise error
+    if not os.path.exists(temp_file):
+        raise Exception("File download failed {}".format(r.text))
+    else:
         shutil.copyfile(temp_file, file_path)
         os.remove(temp_file)
 
