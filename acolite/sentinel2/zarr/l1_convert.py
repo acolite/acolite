Fixed zarr grid rotation and added s2_geometry_plane_fit as setting

acolite/sentinel2/zarr/l1_convert.py
@@ -5,12 +5,12 @@
 ## modifications: 2025-11-17 (QV) added tile merging
 ##                2026-01-14 (QV) changed AUX interpolation and integrated to sentinel2.zarr
 ##                2026-02-18 (QV) added plane_fit_geom as keyword - but don't use it!
+##                2026-02-26 (QV) fixed grid rotation and moved plane_fit_geom to settings as s2_geometry_plane_fit
 
 def l1_convert(inputfile, output = None, settings = None,
                 check_sensor = True,
                 check_time = True,
                 max_merge_time = 600, # seconds,
-                plane_fit_geom = False,
                 ):
 
     import sys, os, glob, dateutil.parser, time
@@ -133,7 +133,8 @@ def l1_convert(inputfile, output = None, settings = None,
         x_grid = z['conditions']['geometry']['x'][:]
         y_grid = z['conditions']['geometry']['y'][:]
         ## set up grid mesh
-        x_grid_mesh, y_grid_mesh = np.meshgrid(x_grid, y_grid, indexing='ij')
+        #x_grid_mesh, y_grid_mesh = np.meshgrid(x_grid, y_grid, indexing='ij')
+        x_grid_mesh, y_grid_mesh = np.meshgrid(x_grid, y_grid, indexing = 'xy')
 
         ## zenith and azimuth indices
         zi = np.where(angle_order == 'zenith')[0][0]
@@ -478,7 +479,7 @@ def l1_convert(inputfile, output = None, settings = None,
                     mean_vza = np.zeros(band_x_mesh.shape) + np.nan
                     mean_vaa = np.zeros(band_x_mesh.shape) + np.nan
 
-                    if plane_fit_geom:
+                    if setu['s2_geometry_plane_fit']:
                         ## plane fitting
                         ## run through detectors
                         for di, det_name in enumerate(detectors):
@@ -574,7 +575,7 @@ def l1_convert(inputfile, output = None, settings = None,
                     band_vza = np.zeros(band_x_mesh.shape) + np.nan
                     band_vaa = np.zeros(band_x_mesh.shape) + np.nan
 
-                    if plane_fit_geom:
+                    if setu['s2_geometry_plane_fit']:
                         ## plane fit
                         ## run through detectors
                         for di, det_name in enumerate(detectors):

acolite/shared/plane_fit.py
@@ -12,7 +12,7 @@ def plane_fit(geom_x, geom_y, geom_grid, mesh = True):
     import scipy
 
     if not mesh:
-        ii, jj = np.meshgrid(geom_x, geom_y, indexing='ij')
+        ii, jj = np.meshgrid(geom_x, geom_y, indexing = 'xy')
         arr = np.vstack([ii.ravel(), jj.ravel(), geom_grid.ravel()])
     else:
         arr = np.vstack([geom_x.ravel(), geom_y.ravel(), geom_grid.ravel()])