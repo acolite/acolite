935f866caef809e84048eff862813a10cc69324e
acolite <quinten.vanhellemont@naturalsciences.be>
2026-06-09 16:55:30 +0200

    Use take_along_axis

--- acolite/shared/array/normalise.py
+++ acolite/shared/array/normalise.py
@@ -3,13 +3,20 @@
 ## written by Quinten Vanhellemont, RBINS
 ## 2026-06-01
 ## modifications: 2026-06-03 (QV) separated linear weight
+##                2026-06-09 (QV) use take_along_axis
 
-def normalise(x, y, x_val, axis = 0):
+def normalise(x, y_, x_val, axis = 0):
     import acolite as ac
+    import numpy as np
+
     idx_0, w0, idx_1, w1 = ac.shared.array.linear_weight(x, x_val)
 
-    if axis == 0:
-        scale_y = y[idx_0, :, :] * w0 + y[idx_1, :, :] * w1
-    else:
-        print('Axis = {} not configured'.format(axis))
+    y = np.asarray(y_)
+    dims = y.shape
+    axis_ = [i for i in range(len(dims))]
+
+    y0 = np.take_along_axis(y, np.expand_dims(idx_0, axis = axis_), axis = axis)
+    y1 = np.take_along_axis(y, np.expand_dims(idx_1, axis = axis_), axis = axis)
+    scale_y = y0 * w0 + y1 * w1
+
     return(y / scale_y)
